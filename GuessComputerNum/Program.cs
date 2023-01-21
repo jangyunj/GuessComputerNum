1908118babdd2ce3ddc8ade2e6504f5316834fc6
Yun Jang <jang.yunj7@gmail.com>
2023-01-20 22:58:43 -0500

    initial commit

---
+++ GuessComputerNum/Program.cs
@@ -0,0 +1,12 @@
+﻿Random random = new Random();
+var computerInput = random.Next(0, 10);
+
+Console.Write("Guess the number the computer is thinking of (1-10): ");
+var userInput = int.Parse(Console.ReadLine());
+
+while (userInput != computerInput)
+{
+    Console.Write("Guess again: ");
+    userInput = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("You've guessed correctly!");
